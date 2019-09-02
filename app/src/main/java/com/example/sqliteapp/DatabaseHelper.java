d1002181c4c6623534ff88eaf93d3bef45de28e2
Amritpal singh Amritpal Singh <aamritpa@sc-2300-50.labs.surrey.sfu.ca>
2019-09-01 19:52:36 -0700

    Adding data

---
+++ app/src/main/java/com/example/sqliteapp/DatabaseHelper.java
@@ -0,0 +1,43 @@
+package com.example.sqliteapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME ="Student.db";
+    public static final String TABLE_NAME ="student_table";
+    public static final String COL_1 ="ID";
+    public static final String COL_2 ="Name";
+    public static final String COL_3 ="Surname";
+    public static final String COL_4 ="Marks";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,SURNAME TEXT,MARKS INTEGER)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(sqLiteDatabase);
+
+    }
+
+    public boolean insertData(String name,String surname, String marks){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,surname);
+        contentValues.put(COL_2,marks);
+
+    }
+}
